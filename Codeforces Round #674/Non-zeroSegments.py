15882a24f9de30aba7d7abcf69a776e4dd82a619
maciejdomagala <mdomagala80@gmail.com>
2021-02-02 19:06:28 +0100

    task

---
+++ Codeforces Round #674/Non-zeroSegments.py
@@ -0,0 +1,71 @@
+import sys
+input = sys.stdin.readline
+import math
+from math import sqrt, floor, ceil
+from collections import Counter
+
+
+############ ---- Input Functions ---- ############
+def inp():
+    return(int(input()))
+def inlt():
+    return(list(map(int,input().split())))
+def insr():
+    s = input()
+    return(list(s[:len(s) - 1]))
+def invr():
+    return(map(int,input().split()))
+def insr2():
+    s = input()
+    return(s.split(" "))
+
+def prime_factorization(n):
+
+    if n == 1:
+        return [1]
+
+    ans=[]
+    i = 2
+    cap = sqrt(n)
+    while i <= cap:
+        if n % i == 0:
+            ans.append(i)
+            n = n//i
+            cap=sqrt(n)
+        else:
+            i += 1
+    if n > 1:
+        ans.append(n)
+    return ans
+
+def binomial(n, k):
+    if n == 1 or n == k:
+        return 1
+
+    if k > n:
+        return 0       
+    else:
+        a = math.factorial(n)
+        b = math.factorial(k)
+        c = math.factorial(n-k)
+        div = a // (b * c)
+        return div  
+
+n = inp()
+nums = invr()
+
+i = 0
+s = {0}
+run = 0
+count = 0
+while i < len(nums):
+    run += nums[i]
+    if run in s:
+        s = {0}
+        count += 1
+        run = 0
+    else:    
+        s.add(run)
+        i += 1
+
+print count
