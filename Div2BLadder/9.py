ea61e2bd77ed00add9b6e2f99f98145f324c75f2
maciejdomagala <mdomagala80@gmail.com>
2021-03-05 09:43:54 +0100

    ladder

---
+++ Div2BLadder/9.py
@@ -0,0 +1,103 @@
+
+from __future__ import division
+import sys
+input = sys.stdin.readline
+import math
+from math import sqrt, floor, ceil
+from collections import Counter
+from copy import deepcopy as dc
+# from statistics import median, mean
+
+
+############ ---- Input Functions ---- ############
+def inp():
+    return(int(input()))
+def inlt():
+    return(list(map(int,input().split())))
+def insr():
+    s = input()
+    return(list(s[:len(s) - 1]))
+def invr():
+    return(map(int,input().split()))
+def insr2():
+    s = raw_input()
+    return(s.split(" "))
+
+def sieve_for_primes_to(n):
+    size = n//2
+    sieve = [1]*size
+    limit = int(n**0.5)
+    for i in range(1,limit):
+        if sieve[i]:
+            val = 2*i+1
+            tmp = ((size-1) - i)//val 
+            sieve[i+val::val] = [0]*tmp
+    return [2] + [i*2+1 for i, v in enumerate(sieve) if v and i>0]
+
+def prime_factorization(n):
+
+    if n == 1:
+        return [1]
+
+    ans=[]
+    i = 2
+    cap = sqrt(n)
+    while i <= cap:
+        if n % i == 0:
+            ans.append(i)
+            n = n//i
+            cap=sqrt(n)
+        else:
+            i += 1
+    if n > 1:
+        ans.append(n)
+    return ans
+
+def binomial(n, k):
+    if n == 1 or n == k:
+        return 1
+
+    if k > n:
+        return 0       
+    else:
+        a = math.factorial(n)
+        b = math.factorial(k)
+        c = math.factorial(n-k)
+        div = a // (b * c)
+        return div 
+
+def small_divisor(n):
+
+    if n % 2 == 0:
+        return 2
+
+    i = 3
+    while i*i <= n:
+        if n%i == 0:
+            return i
+        i += 2
+
+    return n 
+
+def comb(x):
+
+    return x*(x-1)//2
+
+s = insr()
+n = len(s)
+last_even = None
+last = int(s[-1])
+
+for i, a in enumerate(s):
+    if int(a) % 2 == 0:
+        last_even = i
+    if int(a) % 2 == 0 and int(a) < last:
+        s[i], s[-1] = s[-1], s[i]
+        print ''.join(s)
+        break
+else:
+    if last_even is not None:
+        s[last_even], s[-1] = s[-1], s[last_even]
+        print ''.join(s)
+    else:
+        print -1
