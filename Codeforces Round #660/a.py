cb9c5440addf23c50818955654e91a69f4fe58d9
maciejdomagala <mdomagala80@gmail.com>
2021-03-22 23:07:11 +0100

    ssdss

---
+++ Codeforces Round #660/a.py
@@ -0,0 +1,76 @@
+
+from __future__ import division
+import sys
+input = sys.stdin.readline
+import math
+from math import sqrt, floor, ceil
+from collections import Counter
+from copy import deepcopy as dc
+from collections import defaultdict
+
+
+############ ---- Input Functions ---- ############
+def inp():
+    return(int(input()))
+def inlt():
+    return(list(map(int,input().split())))
+def insr():
+    s = input()
+    return(list(s[:len(s) - 1]))
+def invr():
+    return(map(int,input().split()))
+def insr2():
+    s = input()
+    return(s.split(" "))
+
+def prime_factorization(n):
+
+    if n == 1:
+        return [1]
+
+    ans=[]
+    i = 2
+    cap = sqrt(n)
+    while i <= cap:
+        if n % i == 0:
+            ans.append(i)
+            n = n//i
+            cap=sqrt(n)
+        else:
+            i += 1
+    if n > 1:
+        ans.append(n)
+    return ans
+
+def binomial(n, k):
+    if n == 1 or n == k:
+        return 1
+
+    if k > n:
+        return 0       
+    else:
+        a = math.factorial(n)
+        b = math.factorial(k)
+        c = math.factorial(n-k)
+        div = a // (b * c)
+        return div 
+        
+# a = raw_input()
+
+for _ in range(inp()):
+    n = inp()
+
+    ans = [6, 10, 14]
+
+    if n < 31:
+        print 'NO'
+    else:
+        if (n-30) in ans:
+            ans.pop(2)
+            ans.append(15)
+            ans.append(n-31)
+        else:
+            ans.append(n-30)
+        
+        print 'YES'
+        print ' '.join([str(a) for a in ans])
