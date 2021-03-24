967c7ef9deece3e10e11075aff50fc3fcbc31b5e
maciejdomagala <mdomagala80@gmail.com>
2021-03-24 21:45:31 +0100

    zz

---
+++ Codeforces Round #611/c.py
@@ -0,0 +1,83 @@
+
+from __future__ import division
+import sys
+input = sys.stdin.readline
+import math
+from math import sqrt, floor, ceil
+from collections import Counter, defaultdict
+from copy import deepcopy as dc
+# from statistics import median, mean
+
+inf = float('inf')
+############ ---- Input Functions ---- ############
+def inp():
+    return(int(input()))
+def inlt():
+    return(list(map(int,input().split())))
+def insr():
+    s = input()
+    return(list(s[:len(s) - 1]))
+def invr():
+    return(map(int,input().split()))
+def insr2():
+    s = input()
+    return(s.split(" "))
+
+def r():
+    return raw_input()
+
+def prime_factorization(n):
+
+    if n == 1:
+        return [1]
+
+    ans=[]
+    i = 2
+    cap = sqrt(n)
+    while i <= cap:
+        if n % i == 0:
+            ans.append(i)
+            n = n//i
+            cap=sqrt(n)
+        else:
+            i += 1
+    if n > 1:
+        ans.append(n)
+    return ans
+
+def binomial(n, k):
+    if n == 1 or n == k:
+        return 1
+
+    if k > n:
+        return 0       
+    else:
+        a = math.factorial(n)
+        b = math.factorial(k)
+        c = math.factorial(n-k)
+        div = a // (b * c)
+        return div 
+
+# a = r()
+
+n = inp()
+arr = invr()
+
+safe = []
+pro = []
+
+d = defaultdict(int)
+places = []
+
+for i,a in enumerate(arr):
+    if a == 0:
+        places.append(i)
+    else:
+        d[a] += 1
+
+print d
+print places
+
+nums = [num for num, place in d.items() if place == 0]
+
+print nums
